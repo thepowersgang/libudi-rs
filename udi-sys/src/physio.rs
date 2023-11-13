5cf14c9966cd5ca7dc49f6484cf9c90b30214a74
John Hodge <tpg@mutabah.net>
2023-11-13 12:50:10 +0800

    udi-sys - Add the rest of physio

--- udi-sys/src/physio.rs
+++ udi-sys/src/physio.rs
@@ -1,13 +1,33 @@
 use super::*;
 
+// NOTE: IA-ABI doesn't specify this, so just assume u64 is sensible
+pub type udi_busaddr64_t = u64;
+
+pub type udi_dma_handle_t = *mut udi_dma_handle_s;
 pub type udi_dma_constraints_t = *mut udi_dma_constraints_s;
 pub const UDI_NULL_DMA_CONSTRAINTS: udi_dma_constraints_t = ::core::ptr::null_mut();
 
 pub type udi_dma_constraints_attr_t = udi_ubit8_t;
 
 pub type udi_dma_constraints_attr_set_call_t = unsafe extern "C" fn(gcb: *mut udi_cb_t, new_constraints: udi_dma_constraints_t, status: udi_status_t);
+pub type udi_dma_prepare_call_t = unsafe extern "C" fn(gcb: *mut udi_cb_t, new_dma_handle: udi_dma_handle_t);
+pub type udi_dma_buf_map_call_t = unsafe extern "C" fn(gcb: *mut udi_cb_t, scgth: udi_scgth_t, complete: udi_boolean_t, status: udi_status_t);
+pub type udi_dma_mem_alloc_call_t = unsafe extern "C" fn(gcb: *mut udi_cb_t, 
+    new_dma_handle: udi_dma_handle_t,
+    mem_ptr: *mut c_void,
+    actual_gap: udi_size_t,
+    single_element: udi_boolean_t,
+    scgth: *mut udi_scgth_t,
+    must_swap: udi_boolean_t
+);
+pub type udi_dma_sync_call_t = unsafe extern "C" fn(gcb: *mut udi_cb_t);
+pub type udi_dma_scgth_sync_call_t = unsafe extern "C" fn(gcb: *mut udi_cb_t);
+pub type udi_dma_mem_to_buf_call_t = unsafe extern "C" fn(gcb: *mut udi_cb_t, new_dst_buf: *mut udi_buf_t);
 
 extern "C" {
+    pub type udi_dma_constraints_s;
+    pub type udi_dma_handle_s;
+
     pub fn udi_dma_constraints_attr_set(
         callback: udi_dma_constraints_attr_set_call_t,
         gcb: *mut udi_cb_t,
@@ -27,15 +47,69 @@ extern "C" {
         constraints: udi_dma_constraints_t,
         attr_type: udi_dma_constraints_attr_t
         );
+    pub fn udi_dma_constraints_free(constraints: udi_dma_constraints_t);
 
-    pub type udi_dma_constraints_s;
+    pub fn udi_dma_limits(dma_limits: *mut udi_dma_limits_t);
+
+    pub fn udi_dma_prepare(
+        callback: udi_dma_prepare_call_t,
+        gcb: *mut udi_cb_t,
+        constraints: udi_dma_constraints_t,
+        flags: udi_ubit8_t
+    );
+    pub fn udi_dma_buf_map(
+        callback: udi_dma_buf_map_call_t,
+        gcb: *mut udi_cb_t,
+        dma_handle: udi_dma_handle_t,
+        buf: *mut udi_buf_t,
+        offset: udi_size_t,
+        len: udi_size_t,
+        flags: udi_ubit8_t
+    );
+    pub fn udi_dma_buf_unmap(
+        dma_handle: udi_dma_handle_t,
+        new_buf_size: udi_size_t
+    ) -> *mut udi_buf_t;
+    pub fn udi_dma_mem_alloc(
+        callback: udi_dma_mem_alloc_call_t,
+        gcb: *mut udi_cb_t,
+        constraints: udi_dma_constraints_t,
+        flags: udi_ubit8_t,
+        nelements: udi_ubit16_t,
+        element_size: udi_size_t,
+        max_gap: udi_size_t
+    );
+    pub fn udi_dma_sync (
+        callback: udi_dma_sync_call_t,
+        gcb: *mut udi_cb_t,
+        dma_handle: udi_dma_handle_t,
+        offset: udi_size_t,
+        length: udi_size_t,
+        flags: udi_ubit8_t
+    );
+    pub fn udi_dma_scgth_sync(
+        callback: udi_dma_scgth_sync_call_t,
+        gcb: *mut udi_cb_t,
+        dma_handle: udi_dma_handle_t
+    );
+
+    pub fn udi_dma_mem_barrier(dma_handle: udi_dma_handle_t);
+    pub fn udi_dma_free(dma_handle: udi_dma_handle_t);
+    pub fn udi_dma_mem_to_buf(
+        callback: udi_dma_mem_to_buf_call_t,
+        gcb: *mut udi_cb_t,
+        dma_handle: udi_dma_handle_t,
+        src_off: udi_size_t,
+        src_len: udi_size_t,
+        dst_buf: *mut udi_buf_t
+    );
 }
 
 #[repr(C)]
 pub struct udi_dma_constraints_attr_spec_t
 {
-    attr_type: udi_dma_constraints_attr_t,
-    attr_value: udi_ubit32_t,
+    pub attr_type: udi_dma_constraints_attr_t,
+    pub attr_value: udi_ubit32_t,
 }
 
 /* DMA Convenience Attribute Codes */
@@ -71,9 +145,73 @@ pub const UDI_DMA_SLOP_OUT_EXTRA    : udi_dma_constraints_attr_t = 163;
 pub const UDI_DMA_SLOP_BARRIER_BITS : udi_dma_constraints_attr_t = 164;
 
 /* Values for UDI_DMA_SCGTH_ENDIANNESS */
-pub const UDI_DMA_LITTLE_ENDIAN: udi_ubit32_t = 1<<6;
-pub const UDI_DMA_BIG_ENDIAN: udi_ubit32_t = 1<<5;
+//pub const UDI_DMA_LITTLE_ENDIAN: udi_ubit32_t = 1<<6;
+//pub const UDI_DMA_BIG_ENDIAN: udi_ubit32_t = 1<<5;
 /* Values for UDI_DMA_ADDR_FIXED_TYPE */
 pub const UDI_DMA_FIXED_ELEMENT: udi_ubit32_t = 1;
 pub const UDI_DMA_FIXED_LIST   : udi_ubit32_t = 2;
 pub const UDI_DMA_FIXED_VALUE  : udi_ubit32_t = 3;
+
+#[repr(C)]
+pub struct udi_dma_limits_t
+{
+    pub max_legal_contig_alloc: udi_size_t,
+    pub max_safe_contig_alloc: udi_size_t,
+    pub cache_line_size: udi_size_t,
+}
+pub const UDI_DMA_MIN_ALLOC_LIMIT: udi_size_t = 4000;
+
+#[repr(C)]
+#[derive(Clone, Copy)]  // For union
+pub struct udi_scgth_element_32_t
+{
+    pub block_busaddr: udi_ubit32_t,
+    pub block_length: udi_ubit32_t,
+}
+#[repr(C)]
+#[derive(Clone, Copy)]  // For union
+pub struct udi_scgth_element_64_t
+{
+    pub block_busaddr: udi_busaddr64_t,
+    pub block_length: udi_ubit32_t,
+    pub el_reserved: udi_ubit32_t,
+}
+    /* Extension Flag */
+pub const UDI_SCGTH_EXT: udi_ubit32_t = 0x80000000;
+#[repr(C)]
+pub struct udi_scgth_t
+{
+    pub scgth_num_elements: udi_ubit16_t,
+    pub scgth_format: udi_ubit8_t,
+    pub scgth_must_swap: udi_boolean_t,
+    pub scgth_elements: udi_scgth_t_scgth_elements,
+    pub scgth_first_segment: udi_scgth_t_scgth_first_segment,
+}
+#[repr(C)]
+pub union udi_scgth_t_scgth_elements {
+    pub el32p: *mut udi_scgth_element_32_t,
+    pub el64p: *mut udi_scgth_element_64_t,
+}
+#[repr(C)]
+pub union udi_scgth_t_scgth_first_segment {
+    pub el32: udi_scgth_element_32_t,
+    pub el64: udi_scgth_element_64_t,
+}
+/* Values for scgth_format */
+pub const UDI_SCGTH_32           : udi_ubit8_t = 1<<0;
+pub const UDI_SCGTH_64           : udi_ubit8_t = 1<<1;
+pub const UDI_SCGTH_DMA_MAPPED   : udi_ubit8_t = 1<<6;
+pub const UDI_SCGTH_DRIVER_MAPPED: udi_ubit8_t = 1<<7;
+
+/* Values for [udi_dma_*] flags */
+pub const UDI_DMA_OUT: udi_ubit8_t = 1<<2;
+pub const UDI_DMA_IN : udi_ubit8_t = 1<<3;
+pub const UDI_DMA_REWIND : udi_ubit8_t = 1<<4;
+pub const UDI_DMA_BIG_ENDIAN : udi_ubit8_t = 1<<5;
+pub const UDI_DMA_LITTLE_ENDIAN : udi_ubit8_t = 1<<6;
+pub const UDI_DMA_NEVERSWAP : udi_ubit8_t = 1<<7;
+
+/// PIO Handle Layout Element Type Code
+pub const UDI_DL_PIO_HANDLE_T: udi_ubit8_t = 200;
+/// DMA Constraints Handle Layout Element Type Code
+pub const UDI_DL_DMA_CONSTRAINTS_T: udi_ubit8_t = 201;
