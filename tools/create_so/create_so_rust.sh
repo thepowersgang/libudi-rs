04b7dfadca53d46c3dd84dc7618fa0de241eacb6
John Hodge <tpg@mutabah.net>
2023-11-18 21:10:32 +0800

    Fix missing udiprops

--- tools/create_so/create_so_rust.sh
+++ tools/create_so/create_so_rust.sh
@@ -10,16 +10,21 @@ shortname=$(basename $1)
 shortname=${shortname%%.a}
 shortname=${shortname##libudi_}
 
+# re-generate libudi.so
 ld -shared -o $(dirname $0)/libudi.so $(dirname $0)/libudi.ld
+
+# Link the executable
 LD_ARGS="-shared -o $shortname.so -u udi_init_info $1"
+LD_ARGS=$LD_ARGS" -u libudi_rs_udiprops"  # Hacky name in udiprops parser
 LD_ARGS=$LD_ARGS" --gc-sections"
 LD_ARGS=$LD_ARGS" -g"
 LD_ARGS=$LD_ARGS" -T $(dirname $0)/link.ld"
 #LD_ARGS=$LD_ARGS" $(dirname $0)/libudi.so"
 #LD_ARGS=$LD_ARGS" --no-undefined"
-ld $LD_ARGS \
-    --retain-symbols-file=$(dirname $0)/create_so_retain.txt \
-    -Map $shortname.map
+LD_ARGS=$LD_ARGS" --retain-symbols-file=$(dirname $0)/create_so_retain.txt"
+#LD_ARGS=$LD_ARGS" -Map $shortname.map"
+ld $LD_ARGS
+    
 
 OUTFILE=$(realpath $shortname.so)
 cd $(dirname $0)/../fix_elf
