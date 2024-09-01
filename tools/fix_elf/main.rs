e9d679d476da1a75eda14719a1c8ea4458bff666
John Hodge <tpg@mutabah.net>
2024-09-01 09:25:09 +0800

    tools fix_elf - Slight cleanup/commenting

--- tools/fix_elf/main.rs
+++ tools/fix_elf/main.rs
@@ -1,3 +1,11 @@
+//
+// Rewrite an ELF file to convert DT_NEEDED to DT_SYMBOLIC
+//
+
+const DT_NEEDED: u64 = 1;
+const DT_SYMBOLIC: u8 = 16;
+const PT_DYNAMIC: [u8; 4] = [2,0,0,0];
+
 fn main() {
     let infile = {
         let mut a = ::std::env::args_os();
@@ -20,10 +28,11 @@ fn main() {
         panic!("No PT_DYNAMIC");
     };
     for chunk in file_contents[p_offset as usize..][..p_filesize as usize].chunks_mut(16) {
-        let d_tag = u64::from_bytes(&chunk[..8]);
+        let d_tag = u64::from_bytes(&chunk[0..]);
         //let d_value = u64::from_bytes(&chunk[8..]);
-        if d_tag == 1 /*DT_NEEDED*/ {
-            chunk[0] = 16;  // DT_SYMBOLIC, actually useful
+        if d_tag == DT_NEEDED {
+            // Just update the first byte (rest are known to be 0)
+            chunk[0] = DT_SYMBOLIC;
         }
         else if d_tag == 0 {
             break;
@@ -39,8 +48,8 @@ fn main() {
 fn find_pt_dynamic(b_dynamic: &[u8], nphent: u16) -> Option<(u64,u64)> {
     let phents = b_dynamic.chunks(56).take(nphent as usize);
     for phent in phents {
-        if phent[..4] == [2,0,0,0] {
-            // PT_DYNAMIC
+        // PT_DYNAMIC
+        if phent[..4] == PT_DYNAMIC {
             let p_offset = u64::from_bytes(&phent[8..]);
             let p_filesize = u64::from_bytes(&phent[32..]);
             return Some((p_offset, p_filesize));
